88a4e1433162ea57b16eb91193527351ecad67ab
JosefStips <stipsjosef@gmail.com>
2025-03-03 12:44:36 +0100

    fixed express static folder bug + added a / request

--- src/constants/app.ts
+++ src/constants/app.ts
@@ -10,38 +10,47 @@ export class App {
 		let app = express();
 		app.use(cors());
 		app.use(express.json());
-		app.use("/assets", express.static('assets'));
-		
+		app.use('/public', express.static('public'));
+
 		this.inner = app;
 	}
 
-	async get(path: string, fn: (req: Request, res: Response) => Promise<Err | void>) {
+	async get(
+		path: string,
+		fn: (req: Request, res: Response) => Promise<Err | void> | Err | void,
+	) {
 		this.inner.get(path, async (req, res) => {
 			let end = await fn(req, res);
-			
-			if(typeof end == 'object') {
+
+			if (typeof end == 'object') {
 				console.error(end);
 				res.status(end.type).send(end.message);
 			}
 		});
 	}
 
-	async post(path: string, fn: (req: Request, res: Response) => Promise<Err | void>) {
+	async post(
+		path: string,
+		fn: (req: Request, res: Response) => Promise<Err | void>,
+	) {
 		this.inner.post(path, async (req, res) => {
 			let end: Err | void = await fn(req, res);
-			
-			if(typeof end === 'object') {
+
+			if (typeof end === 'object') {
 				console.error(end);
 				res.status(end.type).send(end.message);
 			}
 		});
 	}
 
-	async delete(path: string, fn: (req: Request, res: Response) => Promise<Err | void>) {
+	async delete(
+		path: string,
+		fn: (req: Request, res: Response) => Promise<Err | void>,
+	) {
 		this.inner.delete(path, async (req, res) => {
 			let end: Err | void = await fn(req, res);
 
-			if(typeof end == 'object') {
+			if (typeof end == 'object') {
 				console.error(end);
 				res.status(end.type).send(end.message);
 			}
@@ -50,7 +59,7 @@ export class App {
 
 	listen(port: string) {
 		this.inner.listen(port, () => {
-			console.log("Server listening at port: " + port);
+			console.log('Server listening at port: ' + port);
 		});
 	}
 }
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,13 @@
 import dotenv from 'dotenv';
-import { getTrickList, postTrick, deleteTrick } from "./constants/trickListApi.js";
-import { App } from "./constants/app.js";
-import DbConnection from "./constants/dbConnection.js";
+import {
+	getTrickList,
+	postTrick,
+	deleteTrick,
+} from './constants/trickListApi.js';
+import { App } from './constants/app.js';
+import DbConnection from './constants/dbConnection.js';
+import { Err } from './constants/errors.js';
+import { Response } from 'express';
 
 dotenv.config();
 
@@ -9,12 +15,20 @@ let dbConn = new DbConnection(
 	process.env.DB_HOST!,
 	process.env.DB_NAME!,
 	process.env.DB_USER!,
-	process.env.DB_PASSWORD!
+	process.env.DB_PASSWORD!,
 );
 
 dbConn.connect();
 
-let app: App = new App();  
+let app: App = new App();
+
+function defaultNamespaceRequest(res: Response) {
+	res.send('hello world. This is the crah api.');
+}
+
+app.get('/', (req, res) => {
+	return defaultNamespaceRequest(res);
+});
 
 /*
 e.g
@@ -22,7 +36,7 @@ e.g
 curl http://localhost:3000/api/tricks?userId=1
 */
 
-app.get("/api/tricks", (req, res) => {
+app.get('/api/tricks', (req, res) => {
 	return getTrickList(req, res, dbConn);
 });
 
@@ -32,7 +46,7 @@ e.g
 curl -H 'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxIiwiZXhwIjoxNTE2OTIzOTAyMn0.TEHZNVRuzcIBnxqrVbaxcLiPfV3IFtHynsV7tWQTAEU' -H 'Content-Type: application/json' --data '{"parts": ["fakie", "quad", "whip"], "spots": [0, 3, 4], "date": null}' --request POST 'http://localhost:3000/api/tricks/new'
 */
 
-app.post("/api/tricks/new", (req, res) => {
+app.post('/api/tricks/new', (req, res) => {
 	return postTrick(req, res, dbConn, process.env.CLERK_PEM_PUBLIC_KEY!);
 });
 
@@ -42,7 +56,7 @@ e.g
 curl -H 'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxIiwiaWF0IjoxNTE2MjM5MjM0NjI2MjR9._rbyL-KlSoffzaw4XXsUpnTzThu3oLWaq84QCvScjY8' --request DELETE 'http://localhost:3000/api/tricks/remove?trickid=0'
 */
 
-app.delete("/api/tricks/remove", (req, res) => {
+app.delete('/api/tricks/remove', (req, res) => {
 	return deleteTrick(req, res, dbConn, process.env.CLERK_PEM_PUBLIC_KEY!);
 });
 
