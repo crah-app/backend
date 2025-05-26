a3fb807dd665e5ab86b985df45675dfca419195c
Josef Stips <93494931+josef-stips@users.noreply.github.com>
2025-05-26 11:56:24 +0200

    Update users.ts

--- src/middleware/users.ts
+++ src/middleware/users.ts
@@ -2,6 +2,9 @@ import { Request, Response } from 'express';
 import { createClerkClient } from '@clerk/backend';
 import { Err } from '../constants/errors.js';
 
+// export to /types
+type UserSetting = "setting01" | "setting02";
+
 const clerkClient = createClerkClient({
 	secretKey: process.env.CLERK_SECRET_KEY,
 });
@@ -20,4 +23,25 @@ export async function getUserStats(req: Request, res: Response) {
 	const userStats = await clerkClient.users.getUser(id);
 
 	res.json(userStats);
+}
+
+// user can alter clerk and crah specific information
+// clerk data gets updated on the client directly and the clerk API triggers the /webhook/clerk route
+// this function manages changes to crah specific user data in the db
+export async function updateUser(req: Request, res: Response, db: DBconnection, data: JSON) {
+	
+}
+
+// client alters an account setting (privacy setting, general setting, ...)
+export async function alterAccountSettings(req: Request, res: Response, db: DBconnection, setting: UserSetting) {
+
+	switch(setting) {
+		case "setting01" :
+			alterSetting01(req, res, db, setting);		
+	}
+}
+
+// alter setting "xy"
+export async function alterSetting01(req : Request, res: Response, db:DBconnection, setting: UserSetting) {
+	
 }
